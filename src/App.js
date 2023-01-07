1525cc9a6158a04ef59cb84d5c170f0808cf6c0c
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-07 12:53:31 +0300

    Header section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,40 @@ function App() {
   return (
     <div className="App">
      
+     {/* header logo */}
+     <div className="header_logo"></div>
+
+     {/* search bar */}
+     <div className="header_search"></div>
+
+     {/* nav header */}
+     <div className="header_nav">
+
+      <div className="nav_item">
+        <span className="nav_item_one">Hello Guest</span>
+        <span className="nav_item_two">Signin</span>
+
+      </div>
+
+      <div className="nav_item">
+        <span className="nav_item_one">Your</span>
+        <span className="nav_item_two">Shop</span>
+        
+      </div>
+
+
+      <div className="nav_item">
+      <span className="nav_item_one">Basket</span>
+      <span className="nav_item_two">0</span>
+        
+      </div>
+     </div>
+     
+
+     
+     
+     
+
     </div>
   );
 }
