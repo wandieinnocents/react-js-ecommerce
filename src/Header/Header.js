d10eea39821e5c62fdbe15a7dabb574619801eed
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-10 23:19:16 +0300

    Add links to checkout and home logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,16 +6,21 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
 
+// Links
+import {Link} from "react-router-dom";
+
 function Header() {
 
     return(
         <div className="header">
 
      {/* header logo */}
+     <Link to="/" style={{ textDecoration:"none"}}>
      <div className="header_logo">
      <StorefrontIcon  fontSize="large" className="logo_header_image" />
      <h2 className="header_logo_title">eShop</h2>
      </div>
+     </Link>
 
      {/* search bar */}
      <div className="header_search">
@@ -39,12 +44,18 @@ function Header() {
         
       </div>
 
+
+      {/* basket */}
+
+      <Link to="/checkout" style={{ textDecoration:"none"}}>
       <div className="nav__itemBasket">
         <ShoppingCartIcon   />
         
         <span className="nav_basketCount">0</span>
-        
       </div>
+      </Link>
+
+     
     
 
 
