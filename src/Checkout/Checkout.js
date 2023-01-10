5cf01c1686ecad4f0abc3aa9d7b64aeda0c7d05b
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-10 21:11:11 +0300

    Add checkout page

---
+++ src/Checkout/Checkout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Checkout.css";
+
+
+function Checkout() {
+
+    return(
+        <div className="checkout">
+
+
+        </div>
+    )
+    
+}
+
+export default Checkout;
