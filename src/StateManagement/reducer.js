6a9800ce029fb53989f3352995526cab69f98b01
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-11 17:35:32 +0300

    Add reducer

---
+++ src/StateManagement/reducer.js
@@ -0,0 +1,23 @@
+// here we shall be defining all application level code
+// and to define states and actions to make changes to the state
+
+
+export const initialState = {
+    // empty basket array to store all products
+    //retrieve all products from the basket
+
+    basket:[],
+
+}
+
+// selector
+//state has current state of data
+//action to make the changes to the state
+const reducer = (state,action) => {
+
+    // add methods u want to use to perform state management
+    
+}
+
+
+export default reducer;
