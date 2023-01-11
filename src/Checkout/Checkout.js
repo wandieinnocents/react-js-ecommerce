4e930d338075b0930897b02505a121161e5faa65
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-11 14:19:35 +0300

    Add checkout product

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -6,9 +6,29 @@ function Checkout() {
 
     return(
         <div className="checkout">
+            {/* left checkout  */}
+            <div className="checkout__left">
+                {/* banner */}
+                <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" 
+                alt="shopping banner"
+                className="checkout__ad" />
 
-        <p>hello world</p>
+                {/* heading */}
+                <div>
+                <h2>YOUR CHECKOUT BASKET</h2>
+                </div>
 
+                {/* checkout product */}
+                <CheckoutProduct />
+
+                
+            </div>
+
+
+
+            {/* right checkout  */}
+            <div className="checkout__left">
+            </div>
 
         </div>
     )
---
+++ src/Checkout/CheckoutProduct.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CheckoutProduct.css";
+
+
+function CheckoutProduct() {
+
+    return(
+        <div className="checkout__product">
+           
+        </div>
+    )
+    
+}
+
+export default CheckoutProduct;
